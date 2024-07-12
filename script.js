0d36e3918f55cd4992a171a2d85ea3d19d5017b5
Andy Pak <147354924+PakmanGames@users.noreply.github.com>
2024-07-12 14:20:42 -0400

    Add comments to script.js explaining purpose of certain areas

--- script.js
+++ script.js
@@ -13,70 +13,80 @@ const numberButtons = Array.from(numberBtns);
 
 // Adds event listeners to each number based on which number it is
 for (let i = 0; i < numberButtons.length; i++) {
-    if (numberButtons[i].classList.contains("zero-btn") || numberButtons[i].classList.contains("decimal-btn")) {
-        if (numberButtons[i].textContent === "0") {
-            numberButtons[i].addEventListener("click", () => {
-                console.log(numberButtons[i].textContent);
-                if (display.classList.contains("first")) {
-                    if (operation.first.length !== 0 && operation.first[0] !== 0 && operation.first[1] !== ".") {
-                        operation.first.push(0);
-                        display.textContent = lengthCheck(operation.first.join(""));
-                    } else if (operation.first[1] !== ".") {
-                        operation.first = [0];
-                        display.textContent = lengthCheck(operation.first.join(""));
-                    } else {
-                        operation.second.push(0);
-                        display.textContent = lengthCheck(operation.first.join(""));
-                    }
-                } else if (display.classList.contains("second")) {
-                    if (operation.second.length !== 0 && operation.second[0] !== 0 && operation.second[1] !== ".") {
-                        operation.second.push(0);
-                        display.textContent = lengthCheck(operation.second.join(""));
-                    } else if (operation.second[1] !== ".") {
-                        operation.second = [0];
-                        display.textContent = lengthCheck(operation.second.join(""));
-                    } else {
-                        operation.second.push(0);
-                        display.textContent = lengthCheck(operation.second.join(""));
-                    }
+    if (numberButtons[i].classList.contains("zero-btn") && numberButtons[i].textContent === "0") {
+        // If the number button pressed is zero
+        numberButtons[i].addEventListener("click", () => {
+            if (display.classList.contains("first")) {
+                // Changing the first value
+                if (operation.first.length !== 0 && operation.first[0] !== 0 && operation.first[1] !== ".") {
+                    // If the first value doesn't start with `0` and `.`
+                    operation.first.push(0);
+                    display.textContent = lengthCheck(operation.first.join(""));
+                } else if (operation.first[1] !== ".") {
+                    // If user spams `0` when its already the value
+                    operation.first = [0];
+                    display.textContent = lengthCheck(operation.first.join(""));
+                } else {
+                    operation.first.push(0);
+                    display.textContent = lengthCheck(operation.first.join(""));
                 }
-            });
-        } else if (numberButtons[i].textContent === ".") {
-            numberButtons[i].addEventListener("click", () => {
-                console.log(numberButtons[i].textContent);
-                if (operation.operator === null && display.classList.contains("first") && operation.first.length === 0) {
+            } else if (display.classList.contains("second")) {
+                // Changing the second value
+                if (operation.second.length !== 0 && operation.second[0] !== 0 && operation.second[1] !== ".") {
+                    // If the second value doesn't start with `0` and `.`
+                    operation.second.push(0);
+                    display.textContent = lengthCheck(operation.second.join(""));
+                } else if (operation.second[1] !== ".") {
+                    // If user spams `0` when its already the value
+                    operation.second = [0];
+                    display.textContent = lengthCheck(operation.second.join(""));
+                } else {
+                    operation.second.push(0);
+                    display.textContent = lengthCheck(operation.second.join(""));
+                }
+            }
+        });
+    } else if (numberButtons[i].classList.contains("decimal-btn") && numberButtons[i].textContent === ".") {
+        // If the number button pressed is the decimal button
+        numberButtons[i].addEventListener("click", () => {
+            if (operation.operator === null && display.classList.contains("first")) {
+                // Changing the first value
+                if (operation.first.length === 0) {
+                    // If the first value is empty and user presses decimal first
                     operation.first.push(0, ".");
                     display.textContent = operation.first.join("");
-                } else if (operation.operator === null && display.classList.contains("first")) {
-                    if (! operation.first.some(item => item === "." ? true : false)) {
-                        operation.first.push(".");
-                        display.textContent = operation.first.join("");
-                    }
-                } else if (operation.operator !== null && display.classList.contains("second")) {
-                    if (operation.second.length === 0) {
-                        operation.second.push(0, ".");
-                        display.textContent = operation.second.join("");
-                    } else if (! operation.second.some(item => item === "." ? true : false)) {
-                        operation.second.push(".");
-                        display.textContent = operation.second.join("");
-                    }
+                } else if (! operation.first.some(item => item === "." ? true : false)) {
+                    // Check if the first value already has a decimal
+                    operation.first.push(".");
+                    display.textContent = operation.first.join("");
                 }
-            });
-        }
+            } else if (operation.operator !== null && display.classList.contains("second")) {
+                // Changing the second value
+                if (operation.second.length === 0) {
+                    // If second value is empty and user presses decimal first
+                    operation.second.push(0, ".");
+                    display.textContent = operation.second.join("");
+                } else if (! operation.second.some(item => item === "." ? true : false)) {
+                    // Check if second value already has a decimal
+                    operation.second.push(".");
+                    display.textContent = operation.second.join("");
+                }
+            }
+        });
     } else {
         numberButtons[i].addEventListener("click", () => {
-            console.log(numberButtons[i].textContent);
             if (operation.operator === null && display.classList.contains("first")) {
+                // Changing the first value
                 operation.first.push(numberButtons[i].textContent);
                 display.textContent = operation.first.join("");
             } else if (operation.operator !== null && display.classList.contains("second")) {
-                // If user presses delete after they press an operator
+                // Changing the second value
                 if (operation.second[0] === 0 && operation.second[1] !== ".") {
+                    // Removes 0 in front of number after user presses `Del` after an operator
                     operation.second.forEach(item => item === 0 ? operation.second.shift() : item);
                 }
                 operation.second.push(numberButtons[i].textContent);
                 display.textContent = lengthCheck(operation.second.join(""));
-                console.log(operation);
             }
         });
     }
@@ -88,50 +98,52 @@ const operatorButtons = Array.from(operatorBtns);
 
 // Adds event listeners to each operator based on what operator it is
 for (let i = 0; i < operatorButtons.length; i++) {
-    if (operatorButtons[i].classList.contains("del-btn") || 
-        operatorButtons[i].classList.contains("clear-btn") || 
-        operatorButtons[i].classList.contains("equal-btn")) {
-        if (operatorButtons[i].textContent === "=") {
-            operatorButtons[i].addEventListener("click", () => {
-                if (operation.first.length !== 0 && operation.operator !== null && operation.second.length !== 0) {
-                    display.textContent = lengthCheck(operate(operation));
-                    // Store previous operator incase user presses 'Del' after pressing '='
-                    const prevOperator = operation.operator;
-                    clear(operation);
-                    operation.first = display.textContent.split("");
-                    operation.operator = prevOperator;
-                    display.classList.remove("first");
-                    display.classList.add("second");
-                    console.log(operation); // FOR TESTING
-                }
-            })
-        } else if (operatorButtons[i].textContent === "AC") {
-            operatorButtons[i].addEventListener("click", () => {
+    if (operatorButtons[i].textContent === "=") {
+        // User presses `=`
+        operatorButtons[i].addEventListener("click", () => {
+            if (operation.first.length !== 0 && operation.operator !== null && operation.second.length !== 0) {
+                // Only operates if there are two values and an operator
+                display.textContent = lengthCheck(operate(operation));
+                // Store previous operator incase user presses 'Del' after pressing '='
+                const prevOperator = operation.operator;
                 clear(operation);
-                display.classList.remove("second");
-                if (! display.classList.contains("first")) {
-                    display.classList.add("first");
+                // Sets the answer as the first value in next operation
+                operation.first = display.textContent.split("");
+                operation.operator = prevOperator;
+                display.classList.remove("first");
+                display.classList.add("second");
+            }
+        })
+    } else if (operatorButtons[i].textContent === "AC") {
+        // User presses `AC`
+        operatorButtons[i].addEventListener("click", () => {
+            // Clean reset to starting values
+            clear(operation);
+            display.classList.remove("second");
+            if (! display.classList.contains("first")) {
+                display.classList.add("first");
+            }
+            display.textContent = "0";
+        });
+    } else if (operatorButtons[i].textContent === "Del") {
+        // User presses `Del`
+        operatorButtons[i].addEventListener("click", () => {
+            if (display.classList.contains("first")) {
+                // Changing the first value
+                operation.first.pop();
+                if (operation.first.length === 0) {
+                    operation.first.push(0);
                 }
-                display.textContent = "0";
-                console.log(operation); // FOR TESTING
-            });
-        } else if (operatorButtons[i].textContent === "Del") {
-            operatorButtons[i].addEventListener("click", () => {
-                if (display.classList.contains("first")) {
-                    operation.first.pop();
-                    if (operation.first.length === 0) {
-                        operation.first.push(0);
-                    }
-                    display.textContent = operation.first.join("");
-                } else if (display.classList.contains("second")) {
-                    operation.second.pop();
-                    if (operation.second.length === 0) {
-                        operation.second.push(0);
-                    }
-                    display.textContent = operation.second.join("");
+                display.textContent = operation.first.join("");
+            } else if (display.classList.contains("second")) {
+                // Changing the second value
+                operation.second.pop();
+                if (operation.second.length === 0) {
+                    operation.second.push(0);
                 }
-            })
-        }
+                display.textContent = operation.second.join("");
+            }
+        })
     } else { // The regular operators 'x', '/', '+', '-'
         operatorButtons[i].addEventListener("click", () => {
             if (operation.first.length === 0) {
@@ -140,7 +152,6 @@ for (let i = 0; i < operatorButtons.length; i++) {
                 operation.operator = operatorButtons[i].textContent;
                 display.classList.remove("first");
                 display.classList.add("second");
-                console.log(operation); // FOR TESTING
             } else if (operation.second.length !== 0) {
                 // Performs operation if second value doesn't have length of 0
                 display.textContent = lengthCheck(operate(operation));
@@ -150,16 +161,15 @@ for (let i = 0; i < operatorButtons.length; i++) {
                 operation.operator = operatorButtons[i].textContent;
                 display.classList.remove("first");
                 display.classList.add("second");
-                console.log(operation); // FOR TESTING
             } else {
                 // Otherwise just setup for user to enter second value
                 operation.operator = operatorButtons[i].textContent;
                 display.classList.remove("first");
                 display.classList.add("second");
-                console.log(operation); // FOR TESTING
             }
         });
     }
+    
 }
 
 // CLears any values of operation
